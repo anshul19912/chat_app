7242fa842e0d7835fda063f6117c54365d611403
anshul19912 <anshulsingh19912@gmail.com>
2022-11-29 02:10:49 +0530

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/auth_screen.dart';
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ChatScreen(),
+      theme: ThemeData(
+          primarySwatch: Colors.orange,
+          backgroundColor: Colors.orange,
+          accentColor: Colors.blue,
+          accentColorBrightness: Brightness.dark,
+          buttonTheme: ButtonTheme.of(context).copyWith(
+              buttonColor: Colors.orange,
+              textTheme: ButtonTextTheme.primary,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)))),
+      home: AuthScreen(),
     );
   }
 }
---
+++ lib/widgets/auth/auth_form.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _usernameController = TextEditingController();
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus(); // will close the soft keyboard if open
+
+    if (isValid) {
+      _formKey.currentState!.save();
+      print(_usernameController);
+      print(_emailController);
+      print(_passwordController);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+            child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(labelText: 'Email address'),
+                  validator: (value) {
+                    if (value!.isEmpty || !value.contains('@')) {
+                      return 'Invalid email!';
+                    }
+                  },
+                ),
+                if (!_isLogin)
+                  TextFormField(
+                    controller: _usernameController,
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 4) {
+                        return 'Please enter at least 4 characters';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(labelText: 'Username'),
+                  ),
+                TextFormField(
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value!.isEmpty || value.length < 7) {
+                      return 'Password must be at least 7 characters long';
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Password'),
+                ),
+                if (!_isLogin)
+                  TextFormField(
+                    validator: (value) {
+                      if (value != _passwordController.text) {
+                        return 'Password do not match';
+                      }
+                      return null;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: 'Confirm Password'),
+                  ),
+                SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                    onPressed: _trySubmit,
+                    child: Text(_isLogin ? 'Login' : 'Signup')),
+                TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                    child: Text(_isLogin
+                        ? 'Create new account'
+                        : "I already have an account")),
+              ],
+            ),
+          ),
+        )),
+      ),
+    );
+  }
+}
